e481be3a494a284d78322377a035eb939a3fbeb0
Kelveen Fabian <kelveenfabian@pursuit.org>
2018-12-01 17:34:44 -0500

    initial commit

---
+++ app/src/main/java/org/pursuit/Group_portfolio_HW_TEAM_THE_TRIPLE_THREAT/MainActivity.java
@@ -0,0 +1,13 @@
+package org.pursuit.Group_portfolio_HW_TEAM_THE_TRIPLE_THREAT;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
