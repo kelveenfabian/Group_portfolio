4da3c136a26b0e3ee71b3997b879cb4cd738b521
Kelveen Fabian <kelveenfabian@pursuit.org>
2018-12-03 14:42:08 -0500

    merge my activity with master

---
+++ app/src/main/java/org/pursuit/Group_portfolio_HW_TEAM_THE_TRIPLE_THREAT/JohnActivity.java
@@ -0,0 +1,14 @@
+package org.pursuit.Group_portfolio_HW_TEAM_THE_TRIPLE_THREAT;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class JohnActivity extends AppCompatActivity {
+private TextView txtVw;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_john);
+    }
+}
