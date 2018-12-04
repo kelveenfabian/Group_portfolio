4710d3c3e5c79315e7b006c72c0dfee79dff0fbc
evelynlucas <evelynlucas@pursuit.org>
2018-12-03 22:19:52 -0500

    new commit evelyn

--- app/src/main/java/org/pursuit/Group_portfolio_HW_TEAM_THE_TRIPLE_THREAT/EvelynActivity.java
+++ app/src/main/java/org/pursuit/Group_portfolio_HW_TEAM_THE_TRIPLE_THREAT/EvelynActivity.java
@@ -9,5 +9,7 @@ public class EvelynActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_evelyn);
+
+        //THIS IS A COMMENT
     }
 }
