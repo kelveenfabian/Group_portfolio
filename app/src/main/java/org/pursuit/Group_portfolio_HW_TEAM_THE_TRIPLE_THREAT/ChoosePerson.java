ee0e3c0220e804c6d31b06d4cab1d0e06f26fe92
garganjohn <garganjohn@gmail.com>
2018-12-08 16:15:04 -0500

    john/finally fixed this god damn nav bar

--- app/src/main/java/org/pursuit/Group_portfolio_HW_TEAM_THE_TRIPLE_THREAT/ChoosePerson.java
+++ app/src/main/java/org/pursuit/Group_portfolio_HW_TEAM_THE_TRIPLE_THREAT/ChoosePerson.java
@@ -1,46 +1,90 @@
 package org.pursuit.Group_portfolio_HW_TEAM_THE_TRIPLE_THREAT;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ActionMenuView;
 import android.widget.Button;
+import android.widget.Toolbar;
 
 public class ChoosePerson extends AppCompatActivity {
     private Button johnBtn;
     private Button veenBtn;
     private Button evelynBtn;
+    private DrawerLayout drawerLayout;
+    private NavigationView navView;
+    private android.support.v7.widget.Toolbar toolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_choose_person);
+        setContentView(R.layout.nav_drawer);
 
-        johnBtn = findViewById(R.id.btn_john);
-        johnBtn.setOnClickListener(new View.OnClickListener() {
+        drawerLayout = findViewById(R.id.nav_drawer);
+
+        toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = (ActionBar) getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_launcher_foreground);
+
+
+        navView = findViewById(R.id.nav_view);
+        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(view.getContext(), JohnActivity.class);
-                startActivity(intent);
-            }
-        });
-        
-        veenBtn = findViewById(R.id.btn_kelveen);
-        veenBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(v.getContext(), VeenActivity.class);
-                startActivity(intent);
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case android.R.id.home:
+                        drawerLayout.openDrawer(GravityCompat.START);
+                        return true;
+                    case R.id.nav_john:
+                        moveToJohn();
+                        break;
+                    case R.id.nav_kelveen:
+                        moveToKelveen();
+                        break;
+                    case R.id.nav_evelyn:
+                        moveToEvelyn();
+                        break;
+
+                }
+                return true;
             }
         });
 
-        evelynBtn = findViewById(R.id.btn_Evelyn);
-        evelynBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(ChoosePerson.this, EvelynActivity.class);
-                startActivity(intent);
-            }
-        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    public void moveToJohn() {
+        Intent intent = new Intent(this, JohnActivity.class);
+        startActivity(intent);
+    }
+
+    public void moveToKelveen() {
+        Intent intent = new Intent(this, VeenActivity.class);
+        startActivity(intent);
+    }
+
+    public void moveToEvelyn() {
+        Intent intent = new Intent(this, EvelynActivity.class);
+        startActivity(intent);
     }
 }
