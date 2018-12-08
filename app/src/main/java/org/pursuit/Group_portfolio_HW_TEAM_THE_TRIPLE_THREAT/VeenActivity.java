9e512c06112e768b2d3d4d023a838adde16fb1d9
Kelveen Fabian <kelveenfabian@pursuit.org>
2018-12-08 15:54:00 -0500

    Created conditions for webBrowser intents

--- app/src/main/java/org/pursuit/Group_portfolio_HW_TEAM_THE_TRIPLE_THREAT/VeenActivity.java
+++ app/src/main/java/org/pursuit/Group_portfolio_HW_TEAM_THE_TRIPLE_THREAT/VeenActivity.java
@@ -1,18 +1,24 @@
 package org.pursuit.Group_portfolio_HW_TEAM_THE_TRIPLE_THREAT;
 
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Color;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class VeenActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_veen);
+
 
     }
 
@@ -32,15 +38,32 @@ public class VeenActivity extends AppCompatActivity {
         switch (itemId) {
             case R.id.textBased:
                 String tbWebAddress = getString(R.string.veen_url_one);
-                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(tbWebAddress)));
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(tbWebAddress));
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(getApplicationContext(), R.string.noweb_toast_text, Toast.LENGTH_SHORT).show();
+                }
                 return true;
+
             case R.id.storyApp:
                 String saWebAddress = getString(R.string.veen_url_two);
-                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(saWebAddress)));
+                Intent intent1 = new Intent(Intent.ACTION_VIEW, Uri.parse(saWebAddress));
+                if (intent1.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent1);
+                } else {
+                    Toast.makeText(getApplicationContext(), R.string.noweb_toast_text, Toast.LENGTH_SHORT).show();
+                }
                 return true;
+
             case R.id.bankteller:
                 String btWebAddress = getString(R.string.veen_url_three);
-                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(btWebAddress)));
+                Intent intent2 = new Intent(Intent.ACTION_VIEW, Uri.parse(btWebAddress));
+                if (intent2.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent2);
+                } else {
+                    Toast.makeText(getApplicationContext(), R.string.noweb_toast_text, Toast.LENGTH_SHORT).show();
+                }
                 return true;
         }
         return super.onOptionsItemSelected(item);
